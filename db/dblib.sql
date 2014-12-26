c83e7e2995fec6ce7f87f7c1f921915da4aa4ace
Ardian Webi Kirda <ardianwebikirda@yahoo.com>
2014-12-26 09:14:11 +0700

    DB TErnayar

--- db/dblib.sql
+++ db/dblib.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2014 at 06:44 AM
+-- Generation Time: Dec 26, 2014 at 09:12 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,6 +19,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `dblib`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lihat`()
+BEGIN 
+	select * FROM role_access;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `simpanTrsNoPinjam`(IN noTrsPinjam VARCHAR(15))
+BEGIN 
+	INSERT INTO trs_nopeminjaman (nopeminjaman) SELECT nopeminjaman FROM temp_peminjaman WHERE nopeminjaman=noTrsPinjam LIMIT 1;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `simpanTrsPeminjaman`()
+BEGIN 
+INSERT INTO trs_peminjaman(nopeminjaman,code_anggota,code_buku,tgl_pinjam,tgl_kembali,statusbuku) SELECT nopeminjaman,code_anggota, code_buku,tgl_pinjam,tgl_kembali, statusbuku 
+FROM temp_peminjaman; 
+
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -45,6 +68,32 @@ INSERT INTO `employee` (`employee_id`, `name`, `email`, `telephone`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `role_access`
+--
+CREATE TABLE IF NOT EXISTS `role_access` (
+`id_users` int(3)
+,`username` varchar(50)
+,`password` varchar(100)
+,`name` varchar(50)
+,`sess_id` varchar(100)
+,`active` enum('Y','N')
+,`id_rolemenu` int(11)
+,`id_group` int(11)
+,`id_menu` int(11)
+,`iscreate` enum('Y','N')
+,`isdelete` enum('Y','N')
+,`isupdate` enum('Y','N')
+,`isaccess` enum('Y','N')
+,`isactive` enum('Y','N')
+,`created` timestamp
+,`createdby` varchar(25)
+,`updated` timestamp
+,`updatedby` varchar(25)
+,`name_menu` varchar(50)
+);
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `temp_peminjaman`
 --
 
@@ -52,8 +101,10 @@ CREATE TABLE IF NOT EXISTS `temp_peminjaman` (
 `id_temp` int(11) NOT NULL,
   `code_anggota` varchar(10) NOT NULL,
   `code_buku` varchar(10) NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `tgl` date NOT NULL
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `statusbuku` enum('Running','OutOfDate') DEFAULT 'Running',
+  `nopeminjaman` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -68,19 +119,22 @@ CREATE TABLE IF NOT EXISTS `tm_anggota` (
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(13) NOT NULL,
+  `active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `isrent` enum('Y','N') NOT NULL DEFAULT 'N',
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `createdby` varchar(50) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updatedby` varchar(50) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `tm_anggota`
 --
 
-INSERT INTO `tm_anggota` (`id_anggota`, `code_anggota`, `name`, `email`, `phone`, `created`, `createdby`, `updated`, `updatedby`) VALUES
-(13, 'USR0001', 'H. Muhidin', 'aa@gg.net', '082137288307', '2014-12-20 12:12:10', 'Administrator', '2014-12-20 12:12:10', 'Administrator'),
-(14, 'USR0002', 'H Sulam', 'Sugeng@sugeng.net', '083244567888', '2014-12-20 12:25:11', 'Administrator', '2014-12-20 12:12:11', 'Administrator');
+INSERT INTO `tm_anggota` (`id_anggota`, `code_anggota`, `name`, `email`, `phone`, `active`, `isrent`, `created`, `createdby`, `updated`, `updatedby`) VALUES
+(14, 'USR0001', 'H Sulam', 'Sugeng@sugeng.net', '083244567888', 'Y', 'Y', '2014-12-25 06:38:14', 'Administrator', '2014-12-20 12:12:11', 'Administrator'),
+(15, 'USR0002', 'H. Muhidin', 'muhidin@muhidin.net', '085227890999', 'Y', 'Y', '2014-12-25 06:38:18', 'Administrator', '2014-12-23 00:12:53', 'Administrator'),
+(16, 'USR0003', 'Frediie', 'fredie@fr.net', '0899007654', 'Y', 'Y', '2014-12-25 09:41:08', 'Administrator', '2014-12-25 07:12:59', 'Administrator');
 
 -- --------------------------------------------------------
 
@@ -108,9 +162,9 @@ CREATE TABLE IF NOT EXISTS `tm_buku` (
 --
 
 INSERT INTO `tm_buku` (`id_buku`, `code_author`, `code`, `name`, `poy`, `publisher`, `tgl_datang`, `status`, `created`, `createdby`, `updated`, `updatedby`) VALUES
-(1, 'AUT0001', 'DIG001', 'AutoCAD 2D Fundamental', 2014, 'DigTuts', '2014-11-26', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-12-14 04:12:58', 'Administrator'),
-(2, 'AUT0001', 'DIG002', 'AutoCAD 2D Building', 2014, 'DigTuts', '2014-11-26', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
-(3, 'AUT0001', 'DIG003', 'AutoCAD 3D Buiding', 2014, 'DigTuts', '2014-11-27', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
+(1, 'AUT0001', 'DIG001', 'AutoCAD 2D Fundamental', 2014, 'DigTuts', '2014-11-26', 'Out', '2014-11-27 10:00:00', 'SYSTEM', '2014-12-14 04:12:58', 'Administrator'),
+(2, 'AUT0001', 'DIG002', 'AutoCAD 2D Building', 2014, 'DigTuts', '2014-11-26', 'Out', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
+(3, 'AUT0001', 'DIG003', 'AutoCAD 3D Buiding', 2014, 'DigTuts', '2014-11-27', 'Out', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
 (4, 'AUT0001', 'DIG004', 'Mechanical Dekstop Fundamental', 2014, 'DigTuts', '2014-11-27', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
 (5, 'AUT0001', 'DIG005', 'Mechanical Dekstop Enginee', 2014, 'DigTuts', '2014-11-27', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
 (6, 'AUT0001', 'DIG006', 'Inventor Fundamental', 2014, 'DigTuts', '2014-11-28', 'Ready', '2014-11-27 10:00:00', 'SYSTEM', '2014-11-27 10:00:00', 'SYSTEM'),
@@ -119,7 +173,7 @@ INSERT INTO `tm_buku` (`id_buku`, `code_author`, `code`, `name`, `poy`, `publish
 (10, 'AUT0002', 'DIG009', 'PHP Programming', 2014, 'DigTuts', '2014-12-10', 'Ready', '2014-12-09 17:00:00', 'SYSTEM', '2014-12-15 09:12:34', 'Administrator'),
 (11, 'AUT0002', 'DIG010', 'PHP Webservice', 2014, 'DigTuts', '2014-12-12', 'Ready', '2014-12-15 06:12:23', 'Administrator', '2014-12-15 09:12:51', 'Administrator'),
 (12, 'AUT0002', 'DIG011', 'Extjs Development Cookbook', 2014, 'DigTuts', '2014-12-11', 'Ready', '2014-12-15 06:12:11', 'Administrator', '2014-12-15 09:12:29', 'Administrator'),
-(14, 'AUT0002', 'DIG012', 'Bootstrap Fundamental', 2014, 'DigTuts', '2014-12-19', 'Ready', '2014-12-19 03:12:34', 'Administrator', '2014-12-19 03:12:34', 'Administrator');
+(14, 'AUT0002', 'DIG012', 'Bootstrap Fundamental', 2014, 'DigTuts', '2014-12-19', 'Out', '2014-12-19 03:12:34', 'Administrator', '2014-12-19 03:12:34', 'Administrator');
 
 -- --------------------------------------------------------
 
@@ -152,18 +206,26 @@ INSERT INTO `tm_group` (`id_group`, `nama_group`, `created`, `createdby`, `updat
 
 CREATE TABLE IF NOT EXISTS `tm_menu` (
 `id_menu` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `cls` int(11) NOT NULL,
-  `leaf` int(11) NOT NULL,
-  `icon` int(11) NOT NULL,
+  `name_menu` varchar(50) NOT NULL,
+  `cls` int(11) DEFAULT NULL,
+  `leaf` int(11) DEFAULT NULL,
+  `icon` int(11) DEFAULT NULL,
   `parent` int(11) DEFAULT NULL,
-  `selector` int(11) NOT NULL,
+  `selector` int(11) DEFAULT NULL,
   `isactive` enum('Y','N') NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `createdby` varchar(25) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updatedby` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tm_menu`
+--
+
+INSERT INTO `tm_menu` (`id_menu`, `name_menu`, `cls`, `leaf`, `icon`, `parent`, `selector`, `isactive`, `created`, `createdby`, `updated`, `updatedby`) VALUES
+(1, 'Setting', NULL, NULL, NULL, NULL, NULL, 'Y', '2014-12-21 14:07:32', 'SYSTEM', '0000-00-00 00:00:00', 'SYSTEM'),
+(2, 'Buku', NULL, NULL, NULL, NULL, NULL, 'Y', '2014-12-21 14:07:32', 'SYSTEM', '0000-00-00 00:00:00', 'SYSTEM');
 
 -- --------------------------------------------------------
 
@@ -212,7 +274,53 @@ CREATE TABLE IF NOT EXISTS `tm_users` (
 --
 
 INSERT INTO `tm_users` (`id_users`, `id_group`, `username`, `password`, `name`, `sess_id`, `active`) VALUES
-(2, 1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', '6oi0gq2nkh0vs4r5l1e2j54ae4', 'Y');
+(2, 1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', 'sb3l2i3sjs1oveqaf4ang99tb2', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trs_nopeminjaman`
+--
+
+CREATE TABLE IF NOT EXISTS `trs_nopeminjaman` (
+`id_nopeminjaman` int(11) NOT NULL,
+  `nopeminjaman` varchar(15) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `trs_nopeminjaman`
+--
+
+INSERT INTO `trs_nopeminjaman` (`id_nopeminjaman`, `nopeminjaman`) VALUES
+(6, 'DIG-TRS00001'),
+(7, 'DIG-TRS00002'),
+(8, 'DIG-TRS00003');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trs_peminjaman`
+--
+
+CREATE TABLE IF NOT EXISTS `trs_peminjaman` (
+`id_trspeminjaman` int(11) NOT NULL,
+  `nopeminjaman` varchar(15) NOT NULL,
+  `code_anggota` varchar(15) NOT NULL,
+  `code_buku` varchar(15) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `statusbuku` enum('Running','OutOfDate') NOT NULL DEFAULT 'Running'
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `trs_peminjaman`
+--
+
+INSERT INTO `trs_peminjaman` (`id_trspeminjaman`, `nopeminjaman`, `code_anggota`, `code_buku`, `tgl_pinjam`, `tgl_kembali`, `statusbuku`) VALUES
+(6, 'DIG-TRS00001', 'USR0001', 'DIG001', '2014-12-23', '2014-12-25', 'Running'),
+(7, 'DIG-TRS00001', 'USR0001', 'DIG002', '2014-12-23', '2014-12-25', 'Running'),
+(8, 'DIG-TRS00002', 'USR0002', 'DIG012', '2014-12-25', '2014-12-27', 'Running'),
+(9, 'DIG-TRS00003', 'USR0003', 'DIG003', '2014-12-25', '2014-12-27', 'Running');
 
 -- --------------------------------------------------------
 
@@ -233,7 +341,16 @@ CREATE TABLE IF NOT EXISTS `trs_role_menu` (
   `createdby` varchar(25) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updatedby` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `trs_role_menu`
+--
+
+INSERT INTO `trs_role_menu` (`id_rolemenu`, `id_group`, `id_menu`, `iscreate`, `isdelete`, `isupdate`, `isaccess`, `isactive`, `created`, `createdby`, `updated`, `updatedby`) VALUES
+(1, 1, 1, 'Y', 'Y', 'Y', 'Y', 'Y', '2014-12-21 14:08:35', 'SYSTEM', '0000-00-00 00:00:00', 'SYSTEM'),
+(2, 1, 2, 'Y', 'Y', 'Y', 'Y', 'Y', '2014-12-21 14:08:35', 'SYSTEM', '0000-00-00 00:00:00', 'SYSTEM'),
+(3, 2, 2, 'Y', 'Y', 'Y', 'Y', 'Y', '2014-12-21 14:09:13', 'SYSTEM', '0000-00-00 00:00:00', 'SYSTEM');
 
 -- --------------------------------------------------------
 
@@ -254,11 +371,66 @@ CREATE TABLE IF NOT EXISTS `view_buku_penulis` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `view_pinjam`
+--
+CREATE TABLE IF NOT EXISTS `view_pinjam` (
+`id_temp` int(11)
+,`codeang` varchar(10)
+,`codebook` varchar(10)
+,`namaanggota` varchar(50)
+,`namabuku` varchar(50)
+,`tgl_pinjam` date
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_trspeminjaman`
+--
+CREATE TABLE IF NOT EXISTS `view_trspeminjaman` (
+`code_anggota` varchar(15)
+,`tgl_pinjam` date
+,`tgl_kembali` date
+,`nopeminjaman` varchar(15)
+,`statusbuku` enum('Running','OutOfDate')
+,`code_buku` varchar(15)
+,`nama` varchar(50)
+,`judul` varchar(50)
+);
+-- --------------------------------------------------------
+
+--
+-- Structure for view `role_access`
+--
+DROP TABLE IF EXISTS `role_access`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `role_access` AS select `tm_users`.`id_users` AS `id_users`,`tm_users`.`username` AS `username`,`tm_users`.`password` AS `password`,`tm_users`.`name` AS `name`,`tm_users`.`sess_id` AS `sess_id`,`tm_users`.`active` AS `active`,`trs_role_menu`.`id_rolemenu` AS `id_rolemenu`,`trs_role_menu`.`id_group` AS `id_group`,`trs_role_menu`.`id_menu` AS `id_menu`,`trs_role_menu`.`iscreate` AS `iscreate`,`trs_role_menu`.`isdelete` AS `isdelete`,`trs_role_menu`.`isupdate` AS `isupdate`,`trs_role_menu`.`isaccess` AS `isaccess`,`trs_role_menu`.`isactive` AS `isactive`,`trs_role_menu`.`created` AS `created`,`trs_role_menu`.`createdby` AS `createdby`,`trs_role_menu`.`updated` AS `updated`,`trs_role_menu`.`updatedby` AS `updatedby`,`tm_menu`.`name_menu` AS `name_menu` from ((`tm_users` join `trs_role_menu` on((`tm_users`.`id_group` = `trs_role_menu`.`id_group`))) join `tm_menu` on((`tm_menu`.`id_menu` = `trs_role_menu`.`id_menu`))) where ((`tm_users`.`username` = 'admin') and (`tm_users`.`password` = convert(md5('admin') using latin1)) and (`tm_users`.`active` = 'Y'));
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `view_buku_penulis`
 --
 DROP TABLE IF EXISTS `view_buku_penulis`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_buku_penulis` AS select `tm_buku`.`id_buku` AS `id_buku`,`tm_buku`.`code` AS `code`,`tm_buku`.`name` AS `name`,`tm_buku`.`tgl_datang` AS `tgl_datang`,`tm_buku`.`publisher` AS `publisher`,`tm_buku`.`poy` AS `poy`,`tm_penulis`.`code_author` AS `code_author`,`tm_penulis`.`firstname` AS `firstname`,`tm_penulis`.`lastname` AS `lastname` from (`tm_buku` left join `tm_penulis` on((`tm_buku`.`code_author` = `tm_penulis`.`code_author`)));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_pinjam`
+--
+DROP TABLE IF EXISTS `view_pinjam`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_pinjam` AS select `temp_peminjaman`.`id_temp` AS `id_temp`,`temp_peminjaman`.`code_anggota` AS `codeang`,`temp_peminjaman`.`code_buku` AS `codebook`,`tm_anggota`.`name` AS `namaanggota`,`tm_buku`.`name` AS `namabuku`,`temp_peminjaman`.`tgl_pinjam` AS `tgl_pinjam` from ((`temp_peminjaman` join `tm_anggota` on((`temp_peminjaman`.`code_anggota` = `tm_anggota`.`code_anggota`))) join `tm_buku` on((`temp_peminjaman`.`code_buku` = `tm_buku`.`code`)));
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_trspeminjaman`
+--
+DROP TABLE IF EXISTS `view_trspeminjaman`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_trspeminjaman` AS select `trs_peminjaman`.`code_anggota` AS `code_anggota`,`trs_peminjaman`.`tgl_pinjam` AS `tgl_pinjam`,`trs_peminjaman`.`tgl_kembali` AS `tgl_kembali`,`trs_nopeminjaman`.`nopeminjaman` AS `nopeminjaman`,`trs_peminjaman`.`statusbuku` AS `statusbuku`,`trs_peminjaman`.`code_buku` AS `code_buku`,`tm_anggota`.`name` AS `nama`,`tm_buku`.`name` AS `judul` from (((`trs_nopeminjaman` join `trs_peminjaman` on((`trs_nopeminjaman`.`nopeminjaman` = `trs_peminjaman`.`nopeminjaman`))) join `tm_anggota` on((`trs_peminjaman`.`code_anggota` = `tm_anggota`.`code_anggota`))) join `tm_buku` on((`trs_peminjaman`.`code_buku` = `tm_buku`.`code`)));
 
 --
 -- Indexes for dumped tables
@@ -313,6 +485,18 @@ ALTER TABLE `tm_users`
  ADD PRIMARY KEY (`id_users`), ADD UNIQUE KEY `username` (`username`), ADD KEY `id_role` (`id_group`);
 
 --
+-- Indexes for table `trs_nopeminjaman`
+--
+ALTER TABLE `trs_nopeminjaman`
+ ADD PRIMARY KEY (`id_nopeminjaman`), ADD UNIQUE KEY `nopeminjaman` (`nopeminjaman`);
+
+--
+-- Indexes for table `trs_peminjaman`
+--
+ALTER TABLE `trs_peminjaman`
+ ADD PRIMARY KEY (`id_trspeminjaman`), ADD KEY `index_nopeminjaman` (`nopeminjaman`);
+
+--
 -- Indexes for table `trs_role_menu`
 --
 ALTER TABLE `trs_role_menu`
@@ -336,7 +520,7 @@ MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `tm_anggota`
 --
 ALTER TABLE `tm_anggota`
-MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `tm_buku`
 --
@@ -351,7 +535,7 @@ MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `tm_menu`
 --
 ALTER TABLE `tm_menu`
-MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tm_penulis`
 --
@@ -363,10 +547,20 @@ MODIFY `id_author` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 ALTER TABLE `tm_users`
 MODIFY `id_users` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT for table `trs_nopeminjaman`
+--
+ALTER TABLE `trs_nopeminjaman`
+MODIFY `id_nopeminjaman` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `trs_peminjaman`
+--
+ALTER TABLE `trs_peminjaman`
+MODIFY `id_trspeminjaman` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
 -- AUTO_INCREMENT for table `trs_role_menu`
 --
 ALTER TABLE `trs_role_menu`
-MODIFY `id_rolemenu` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id_rolemenu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -389,6 +583,12 @@ ADD CONSTRAINT `fkey_author` FOREIGN KEY (`code_author`) REFERENCES `tm_penulis`
 --
 ALTER TABLE `tm_users`
 ADD CONSTRAINT `fk_user_group` FOREIGN KEY (`id_group`) REFERENCES `tm_group` (`id_group`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `trs_peminjaman`
+--
+ALTER TABLE `trs_peminjaman`
+ADD CONSTRAINT `fk_trspeminjaman` FOREIGN KEY (`nopeminjaman`) REFERENCES `trs_nopeminjaman` (`nopeminjaman`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `trs_role_menu`
